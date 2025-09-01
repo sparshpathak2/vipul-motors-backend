c50958b3b63822adee2b3f852d5318849015b982
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2025-09-01 18:42:12 +0530

    Remove old migration files

--- prisma/migrations/20250830085900_removed_active_flag/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isActive` on the `Banner` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Banner" DROP COLUMN "isActive";
--- prisma/migrations/20250830202724_added_imageurl_to_schemas/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Banner" ADD COLUMN     "imageUrl" TEXT;
-
--- AlterTable
-ALTER TABLE "TrueValueModel" ADD COLUMN     "imageUrl" TEXT;
