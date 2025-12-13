a5b85cbd07f67883fc202eecc4084bb595d87b4d
Sparsh <sparshpathak2.0@gmail.com>
2025-12-13 11:15:09 +0530

    fixing ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
             name: "vipul-motors-server",
 
             script: "src/index.js",
-            cwd: "/home/ubuntu/vipul-motors/vipul-motors-api-server",
+            cwd: "/home/ubuntu/vipul-motors/vipul-motors-backend",
 
             // ---- Security ----
             watch: false,
